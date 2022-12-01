79a178bfc4df812474cd4cf01ef57b509d0c0a35
Mateo Difranco <mateodifr@gmail.com>
2022-12-01 12:58:57 -0300

    First two solutions

---
+++ one/main.clj
@@ -0,0 +1,26 @@
+(ns aoc22.one.main
+  (:require [clojure.string :as str]))
+
+(def input
+  (str/split (slurp "input") #"\n\n"))
+
+(defn get-calories [coll]
+  (->> (str/split coll #"\n")
+       (map #(Integer/parseInt %))))
+
+(defn get-smurf-calories [coll]
+ (map #(reduce + (get-calories %)) coll))
+
+(defn fatest-smurf [coll]
+  (apply max (get-smurf-calories coll)))
+
+(defn fatest-n-smurfs [n coll]
+  (->> (get-smurf-calories coll)
+       (sort >)
+       (take n)
+       (reduce +)))
+
+(comment
+  (fatest-smurf input)
+  (fatest-n-smurfs 3 input)
+  )
